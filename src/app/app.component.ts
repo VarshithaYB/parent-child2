c113f5547b2971b11828a24d2c894a491f268864
VarshithaYB <varshithabasavesh03@gmail.com>
2024-10-30 18:01:42 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'parentchild2';
+
+  parentItems :string[]=[];
+  onChildEvent(newItem: string)
+  {
+    this.parentItems.push(newItem)
+  }
 }
